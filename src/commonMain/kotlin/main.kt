d9c6850761e2e7f33647dfa8824819317e9c563e
Piotr Krzeminski <git@krzeminski.it>
2020-12-16 06:06:17 +0100

    Clean up

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,49 +1,48 @@
-import com.soywiz.kds.*
-import com.soywiz.klock.*
-import com.soywiz.korge.*
-import com.soywiz.korge.scene.*
-import com.soywiz.korge.tween.*
-import com.soywiz.korge.view.*
-import com.soywiz.korge3d.*
-import com.soywiz.korge3d.format.*
-import com.soywiz.korge3d.tween.moveBy
-import com.soywiz.korim.color.*
-import com.soywiz.korinject.*
-import com.soywiz.korio.async.*
-import com.soywiz.korio.file.std.*
-import com.soywiz.korma.geom.*
-import com.soywiz.korma.interpolation.*
+import com.soywiz.klock.seconds
+import com.soywiz.korge.Korge
+import com.soywiz.korge.scene.KorgeModule
+import com.soywiz.korge.scene.Scene
+import com.soywiz.korge.tween.get
+import com.soywiz.korge.tween.tween
+import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.addUpdater
+import com.soywiz.korge3d.Korge3DExperimental
+import com.soywiz.korge3d.format.readColladaLibrary
+import com.soywiz.korge3d.get
+import com.soywiz.korge3d.instantiate
+import com.soywiz.korge3d.light
+import com.soywiz.korge3d.position
+import com.soywiz.korge3d.positionLookingAt
+import com.soywiz.korge3d.scene3D
+import com.soywiz.korim.color.Colors
+import com.soywiz.korim.color.RGBA
+import com.soywiz.korinject.AsyncInjector
+import com.soywiz.korio.async.launchImmediately
+import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.Angle
+import com.soywiz.korma.geom.SizeInt
+import com.soywiz.korma.geom.cos
+import com.soywiz.korma.geom.degrees
+import com.soywiz.korma.geom.sin
+import com.soywiz.korma.geom.times
+import com.soywiz.korma.interpolation.Easing
 
-//suspend fun main() = Demo3.main(args)
-suspend fun main() = Korge(Korge.Config(module = Korge3DSampleModule()))
+suspend fun main() = Korge(Korge.Config(module = StairLiftSimulatorModule()))
 
 @Korge3DExperimental
-class Korge3DSampleModule : KorgeModule(RootScene::class) {
+class StairLiftSimulatorModule : KorgeModule(RootScene::class) {
     override val size: SizeInt = SizeInt(1280, 720)
     override val title: String = "KorGE 3D"
     override val bgcolor: RGBA = RGBA.float(.25f, .25f, .25f, 1f)
 
     override suspend fun AsyncInjector.configure() {
         mapPrototype { RootScene() }
-        mapPrototype { MonkeyScene() }
-    }
-}
-
-class RootScene : Scene() {
-    lateinit var contentSceneContainer: SceneContainer
-
-    override suspend fun Container.sceneInit() {
-        contentSceneContainer = sceneContainer(views)
-
-        contentSceneContainer.changeTo<MonkeyScene>()
     }
 }
 
 @Korge3DExperimental
-class MonkeyScene : Scene() {
+class RootScene : Scene() {
     override suspend fun Container.sceneInit() {
-        //delay(10.seconds)
-        //println("delay")
         scene3D {
             val light1 = light().position(0, 10, +10).setTo(Colors.RED)
             val light2 = light().position(10, 0, +10).setTo(Colors.BLUE)
@@ -70,8 +69,6 @@ class MonkeyScene : Scene() {
                 sideFlap?.rotationX = Angle.fromDegrees(tick)
                 tick++
             }
-
         }
     }
-
 }
