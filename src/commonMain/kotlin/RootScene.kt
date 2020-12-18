0dc1c618fe1a3e8e79137c8e1d8924cc3ac884a2
Piotr Krzeminski <git@krzeminski.it>
2020-12-18 06:18:58 +0100

    Animate all vital parts of the model

--- src/commonMain/kotlin/RootScene.kt
+++ src/commonMain/kotlin/RootScene.kt
@@ -8,7 +8,7 @@ import com.soywiz.korge.view.position
 import com.soywiz.korge3d.Camera3D
 import com.soywiz.korge3d.Korge3DExperimental
 import com.soywiz.korge3d.Library3D
-import com.soywiz.korge3d.animation.Animation3D
+import com.soywiz.korge3d.View3D
 import com.soywiz.korge3d.findByType
 import com.soywiz.korge3d.format.readColladaLibrary
 import com.soywiz.korge3d.instantiate
@@ -24,12 +24,11 @@ class RootScene : Scene() {
             val mainSceneView = library.mainScene.instantiate()
             this += mainSceneView
 
-            val drivingUpAndDownAnimation = library.getAnimation("Driving_up_and_down_Driving_up_and_downAction_transform")
-            val drivingUpAndDownAnimator = RandomAccessAnimator(drivingUpAndDownAnimation, mainSceneView)
-            val lowerFlapAnimation = library.getAnimation("Lower_flap_Lower_flapAction_transform")
-            val lowerFlapAnimator = RandomAccessAnimator(lowerFlapAnimation, mainSceneView)
-            val higherFlapAnimation = library.getAnimation("Higher_flap_Higher_flapAction_transform")
-            val higherFlapAnimator = RandomAccessAnimator(higherFlapAnimation, mainSceneView)
+            val drivingUpAndDownAnimator = library.getAnimator("Driving_up_and_down_Driving_up_and_downAction_transform", mainSceneView)
+            val foldablePlatformAnimator = library.getAnimator("Foldable_platform_Foldable_platformAction_transform", mainSceneView)
+            val lowerFlapAnimator = library.getAnimator("Lower_flap_Lower_flapAction_transform", mainSceneView)
+            val higherFlapAnimator = library.getAnimator("Higher_flap_Higher_flapAction_transform", mainSceneView)
+            val barriersAnimator = library.getAnimator("Barriers_BarriersAction_transform", mainSceneView)
 
             val cameraFromModel = mainSceneView.findByType<Camera3D>().firstOrNull()
                     ?: throw RuntimeException("Camera not found in the model")
@@ -37,8 +36,10 @@ class RootScene : Scene() {
 
             val movingParts = MovingParts(
                     drivingUpAndDownAnimator = drivingUpAndDownAnimator,
+                    foldablePlatformAnimator = foldablePlatformAnimator,
                     lowerFlapAnimator = lowerFlapAnimator,
                     higherFlapAnimator = higherFlapAnimator,
+                    barriersAnimator = barriersAnimator,
             )
             val controllerLogic = ControllerLogic()
 
@@ -65,15 +66,18 @@ class RootScene : Scene() {
 }
 
 @Korge3DExperimental
-private fun Library3D.getAnimation(name: String): Animation3D {
-    return animationDefs[name] ?: throw IllegalArgumentException("Could not find animation with name '$name'")
+private fun Library3D.getAnimator(name: String, mainSceneView: View3D): RandomAccessAnimator {
+    val animation = animationDefs[name] ?: throw IllegalArgumentException("Could not find animation with name '$name'")
+    return RandomAccessAnimator(animation, mainSceneView)
 }
 
 @Korge3DExperimental
 data class MovingParts(
         val drivingUpAndDownAnimator: RandomAccessAnimator,
+        val foldablePlatformAnimator: RandomAccessAnimator,
         val lowerFlapAnimator: RandomAccessAnimator,
         val higherFlapAnimator: RandomAccessAnimator,
+        val barriersAnimator: RandomAccessAnimator,
 )
 
 @Korge3DExperimental
@@ -86,6 +90,8 @@ private fun MovingParts.prepareSensorInputs(): SensorInputs {
 @Korge3DExperimental
 private fun MovingParts.executeActuatorOutputs(actuatorOutputs: ActuatorOutputs, timeSpan: TimeSpan) {
     drivingUpAndDownAnimator.progress += actuatorOutputs.mainMotorSpeed * timeSpan.seconds.toFloat()
+    foldablePlatformAnimator.progress += actuatorOutputs.mainMotorSpeed * timeSpan.seconds.toFloat()
     lowerFlapAnimator.progress += actuatorOutputs.mainMotorSpeed * timeSpan.seconds.toFloat()
     higherFlapAnimator.progress += actuatorOutputs.mainMotorSpeed * timeSpan.seconds.toFloat()
+    barriersAnimator.progress += actuatorOutputs.mainMotorSpeed * timeSpan.seconds.toFloat()
 }
